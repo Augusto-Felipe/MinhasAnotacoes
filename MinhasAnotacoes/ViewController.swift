2f489ed4138f3dadfdbe778afdee583cb7a604af
Felipe Augusto <augusto_felipe@icloud.com>
2023-02-01 17:47:16 -0300

    First Commit

--- MinhasAnotacoes/ViewController.swift
+++ MinhasAnotacoes/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        textView.text = recuperarAnotacao()
+        
     }
 
-
+    @IBAction func botaoSalvarPressionado(_ sender: UIButton) {
+        if let texto = textView.text {
+            UserDefaults.standard.set(texto, forKey: "anotacao")
+        }
+    }
+    
+    func recuperarAnotacao() -> String {
+        if let anotacaoSalva = UserDefaults.standard.object(forKey: "anotacao") {
+            return anotacaoSalva as! String
+        }
+        
+        return ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
